dcbbd6d0578a0248722dd415ec84dfc42f96f1c9
Umar <muhammadshafqatali3@gmail.com>
2023-07-28 02:59:31 +0500

    sdfsdF

--- src/Components/card.js
+++ src/Components/card.js
@@ -1,15 +1,22 @@
 import React from "react";
 
 const Card = ({ images }) => {
+  const handleDownload = (url) => {
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "umar_image.jpg";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
   return (
     <div className="image-gallery">
       {images.map((image) => (
-        <div className="card">
-          <img
-            key={image.id}
-            src={image.urls.small}
-            alt={image.alt_description}
-          />
+        <div className="card" key={image.id}>
+          <img src={image.urls.regular} alt={image.alt_description} />
+          <button onClick={() => handleDownload(image.links.download)}>
+            Download
+          </button>
         </div>
       ))}
     </div>
